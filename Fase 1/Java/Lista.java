02af3adc9450b32dec43e06aa24188d02bf2f4f5
Pablo <pablo.silva61@fatec.sp.gov.br>
2024-11-25 11:16:55 -0300

    removendo alguns comentarios

--- Fase 1/Java/Lista.java
+++ Fase 1/Java/Lista.java
@@ -1,26 +1,23 @@
 package org.example;
 
-// define um nó que contém o nome e uma referência ao próximo elemento da fila
 class Lista {
     String nome;
-    Lista proximo; // ponteiro para o próximo nó na fila
+    Lista proximo;
 
     public Lista(String nome) {
-        this.nome = nome; // inicializa o nó com o nome recebido
-        this.proximo = null; // inicializa a referência para o próximo nó como null
+        this.nome = nome; 
+        this.proximo = null; 
     }
 }
 
-// implementa os métodos de uma fila
 class Fila {
-    // adiciona uma nova pessoa ao final da fila
     public Lista adicionar(String nome, Lista primeiro, Lista ultimo) {
-        Lista novo = new Lista(nome); // cria um novo nó
-        if (primeiro == null) { // verifica se a fila está vazia
-            primeiro = novo; // atualiza o início da fila
-            ultimo = novo; // atualiza o final da fila
+        Lista novo = new Lista(nome);
+        if (primeiro == null) {
+            primeiro = novo;
+            ultimo = novo;
         } else {
-            ultimo.proximo = novo; // liga o último nó ao novo
+            ultimo.proximo = novo;
             ultimo = novo; // atualiza o último nó
         }
         return primeiro; // retorna o novo início da fila
@@ -28,7 +25,7 @@ class Fila {
 
     // remove a primeira pessoa da fila
     public Lista remover(Lista primeiro, Lista ultimo) {
-        if (primeiro == null) { // verifica se a fila está vazia
+        if (primeiro == null) {
             throw new IllegalStateException("Fila vazia");
         }
         primeiro = primeiro.proximo; // atualiza o início para o próximo nó
