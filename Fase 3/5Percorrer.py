1919a4cfef577606ff6bd96077ea037954a4dd67
Pablo <143049920+pablohgs05@users.noreply.github.com>
2024-12-02 17:13:26 -0300

    Update 5Percorrer.py
    Pré-ordem, Em ordem e Pós-ordem

--- Fase 3/5Percorrer.py
+++ Fase 3/5Percorrer.py
@@ -1,8 +1,26 @@
-    def percorrer_em_ordem(self):
-        self.percorrer_recursivo(self.raiz)
+def percorrer_pre_ordem(self):
+    self.percorrer_recursivo_pre_ordem(self.raiz)
 
-    def percorrer_recursivo(self, atual):
-        if atual is not None:  # verifica se há um nó
-            self.percorrer_recursivo(atual.esquerda)  # visita a esquerda
-            print(atual.valor, end=" ")  # imprime o nó atual
-            self.percorrer_recursivo(atual.direita)  # visita a direita
+def percorrer_recursivo_pre_ordem(self, atual):
+    if atual is not None:
+        print(atual.valor, end=" ")  # processa o nó atual
+        self.percorrer_recursivo_pre_ordem(atual.esquerda)  # visita a esquerda
+        self.percorrer_recursivo_pre_ordem(atual.direita)  # visita a direita
+
+def percorrer_em_ordem(self):
+    self.percorrer_recursivo_em_ordem(self.raiz)
+
+def percorrer_recursivo_em_ordem(self, atual):
+    if atual is not None:
+        self.percorrer_recursivo_em_ordem(atual.esquerda)  # visita a esquerda
+        print(atual.valor, end=" ")  # processa o nó atual
+        self.percorrer_recursivo_em_ordem(atual.direita)  # visita a direita
+
+def percorrer_pos_ordem(self):
+    self.percorrer_recursivo_pos_ordem(self.raiz)
+
+def percorrer_recursivo_pos_ordem(self, atual):
+    if atual is not None:
+        self.percorrer_recursivo_pos_ordem(atual.esquerda)  # visita a esquerda
+        self.percorrer_recursivo_pos_ordem(atual.direita)  # visita a direita
+        print(atual.valor, end=" ")  # processa o nó atual
